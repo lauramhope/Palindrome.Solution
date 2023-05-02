def2abd6dcbeedc302e24358d2cb3db5ecc13782
Laura Hope <ssteamlaura@gmail.com>
2023-05-02 15:26:48 -0700

    Remove .Reverse() method and added a List for PalindromeChecker:

--- Palindrome/Models/ClassName.cs
+++ Palindrome/Models/ClassName.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Palindrome.Models
 {
@@ -6,9 +7,14 @@ namespace Palindrome.Models
   {
     public bool IsPalindrome(string input)
     {
-      char[] inputArray = input.ToCharArray();      
-      Array.Reverse(inputArray);
-      string reversedInput = String.Join("", inputArray);      
+      char[] inputArray = input.ToCharArray();
+      List<char> reversedList = new List<char> {};
+      foreach (char element in inputArray)
+      {
+        reversedList.Insert(0, element);
+      }
+      //Array.Reverse(inputArray);
+      string reversedInput = String.Join("", reversedList);      
       if (input == reversedInput)
       {
         return true;
